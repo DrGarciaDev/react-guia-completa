1772d2de866d7bf95c61d9a5ca44205071fb25a8
Luis García <luis_grdz@hotmail.com>
2019-12-23 21:27:07 -0600

    Video 28: Heredando de propiedades y metodos de una clas

--- js/app.js
+++ js/app.js
@@ -310,12 +310,26 @@ class Tarea{
     }
     // metodos
     mostrar(){
-        return `${this.nombre} tiene una prioridad ${this.prioridad}`;
+        console.log(`${this.nombre} tiene una prioridad ${this.prioridad}`);
+    }
+}
+
+class ComprasPendientes extends Tarea {
+    constructor(nombre, prioridad, cantidad){
+        super(nombre, prioridad);
+        this.cantidad = cantidad;
+    }
+    mostrar(){
+        super.mostrar();
+        console.log(` y la cantidad de ${this.cantidad}`);
     }
 }
 
 let tarea1 = new Tarea('Aprendiendo javascript', 'Alta');
 let tarea2 = new Tarea('Aprendiendo python', 'Alta');
 
-console.log(tarea1.mostrar());
-console.log(tarea2.mostrar());
+// console.log(tarea1.mostrar());
+// console.log(tarea2.mostrar());
+
+let compra1 = new ComprasPendientes('Jabón', 'Urgente', 3);
+compra1.mostrar();
